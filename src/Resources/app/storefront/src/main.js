ae6c3c6c22e822e5487e347c9bf4cd9e9bc4a2b6
Julius Noack <julius.noack@netlogix.de>
2024-08-20 17:38:33 +0200

    feat: add first prototype of variant listing gallery

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,10 +1,15 @@
 import VariantHoverSwitchPlugin from './plugin/variant-hover-switch.plugin';
 import OffCanvasCartSwitchOptionPlugin from "./plugin/offcanvas-cart-switch-option.plugin";
+import VariantListingGallery from "./plugin/variant-listing-gallery";
 
 const PluginManager = window.PluginManager;
 
 if (window.sasShowOnProductCard) {
     PluginManager.register('VariantHoverSwitch', VariantHoverSwitchPlugin, '[data-variant-hover-switch]');
+}
+
+if (window.sasShowGalleryOnListingPage) {
+    PluginManager.register('VariantListingGallery', VariantListingGallery, '.product-box.box-standard');
 }
 
 if (window.sasShowOnOffCanvasCart) {
