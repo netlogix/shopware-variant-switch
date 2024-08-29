8e52f01ef1bffca38a51c251dde0e454293ff2fc
Jonas Wrosch <Jonas.Wrosch@netlogix.de>
2024-08-29 07:42:36 +0200

    fix: JS Event in wishlist-extension is not being catched
    Since Shopwar JS events are only available in the plugin-emitter that they are thrown in
    
    Related: MOSHOP-191

--- src/Resources/app/storefront/src/plugin/wishlist-extension.plugin.js
+++ src/Resources/app/storefront/src/plugin/wishlist-extension.plugin.js
@@ -2,9 +2,11 @@ import Plugin from 'src/plugin-system/plugin.class';
 
 export default class WishlistExtensionPlugin extends Plugin {
     init() {
-        this.$emitter.subscribe(
+        const plugin = window.PluginManager.getPluginInstanceFromElement(document.querySelector('[data-wishlist-storage]'), 'WishlistStorage');
+        plugin.$emitter.subscribe(
             'Wishlist/onProductRemoved',
             () => {
+                console.log('Product removed from wishlist with window.PluginManager.getPlugin');
                 window.location.reload();
             }
         );
